ea6cf8871143fa8badc5c9acb0cc66aaa227649e
Dalton Hubble <dghubble@gmail.com>
2013-03-15 03:01:57 -0400

    Modifications to aws fabfile. Running tests 20 times each on 4 EC2 instances (should take about an hour). Over about 90 tests, only 1 failure has been observed in the final test because of a wrong value (possibly the rare bug observed in the Paxos implementation which occurred in about 1/200 cases in the 3A test suite).

--- src/kvpaxos/testing/aws/fabfile.py
+++ src/kvpaxos/testing/aws/fabfile.py
@@ -1,22 +1,30 @@
 from __future__ import with_statement
-from fabric.api import run, env, local
+from fabric.api import run, env, local, parallel
 from fabric.context_managers import cd
 from fabric.contrib.console import confirm
 
-env.hosts = ['ec2-54-234-223-49.compute-1.amazonaws.com']
+#'ec2-54-234-223-49.compute-1.amazonaws.com'
+env.hosts = ['ec2-54-234-252-48.compute-1.amazonaws.com', 'ec2-50-17-105-37.compute-1.amazonaws.com', 'ec2-54-242-171-198.compute-1.amazonaws.com', 'ec2-54-242-213-128.compute-1.amazonaws.com']
 env.user = 'ubuntu'
 env.forward_agent = True
 
+@parallel
 def git_pull():
 	run("git clone git@github.com:dghubble/6.824-labs.git && ls")
 	position_for_test()
 
+@parallel
+def git_update():
+	run("cd 6.824-labs && git pull origin master")
+	position_for_test()
 
 def position_for_test():
 	run("cd 6.824-labs/src && export GOPATH=$HOME/6.824-labs && cd kvpaxos && go test -i")
 
+@parallel
 def simple_test():
 	run("cd 6.824-labs/src && export GOPATH=$HOME/6.824-labs && cd kvpaxos && go test -i && go test > debug.log 2> error.log")
 
+@parallel
 def medium_round():
 	run("cd 6.824-labs/src && export GOPATH=$HOME/6.824-labs && cd kvpaxos && go test -i && bash repeat_test.sh > test/test.log")
